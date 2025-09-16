f5c506e24ad23b64076884009e5088e531a21b68
sypatrick <booth20037@gmail.com>
2025-09-16 22:45:50 +0900

    open ai 테스트

--- src/main/java/org/example/spai/domain/openai/controller/ChatController.java
+++ src/main/java/org/example/spai/domain/openai/controller/ChatController.java
@@ -2,25 +2,31 @@ package org.example.spai.domain.openai.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.spai.domain.openai.service.OpenAiService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.util.Map;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ChatController {
     private final OpenAiService openAiService;
 
+    @ResponseBody
     @PostMapping("/chat")
     public String chat(@RequestBody Map<String, String> body){
         return openAiService.generate(body.get("text"));
     }
 
+    @ResponseBody
     @PostMapping("/chat/stream")
     public Flux<String> streamChat(@RequestBody Map<String, String> body){
         return openAiService.generateStream(body.get("text"));
+    }
+
+    @GetMapping("/")
+    public String chatPage(){
+        return "chat";
     }
 }
